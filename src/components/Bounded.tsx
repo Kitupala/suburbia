93c1c18933dff6a42e72f663719c5e69dfe6a965
Kitupala <kimmo.tuupanen@gmail.com>
2025-02-18 11:19:02 +0200

    Add @ts-expect-error comments

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -15,7 +15,9 @@ export function Bounded({
   ...restProps
 }: BoundedProps) {
   return (
+    // @ts-expect-error nothing here
     <Comp
+      // @ts-expect-error nothing here
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
         className,
